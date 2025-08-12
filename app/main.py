3182a094c79cc89b923f2b6efca8c4df5295ba4b
Chen, Danny <3949245+danny50610@users.noreply.github.com>
2025-08-12 14:46:04 +0800

    feat: impl login api

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
-from typing import Annotated
+from datetime import datetime, timedelta, timezone
+import secrets
+from typing import Annotated, Any
 
 from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.security import OAuth2PasswordRequestForm
+import jwt
 from sqlmodel import Session, SQLModel, create_engine, select
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from passlib.context import CryptContext
 
-from app.models import Hero
+from app.models import User, ShortLink, Token
 
 
 class Settings(BaseSettings):
     postgres_url: str = ''
+    SECRET_KEY: str = secrets.token_urlsafe(32)
 
     model_config = SettingsConfigDict(env_file=".env")
 
@@ -37,37 +43,79 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/heroes/")
-def create_hero(hero: Hero, session: SessionDep) -> Hero:
-    session.add(hero)
-    session.commit()
-    session.refresh(hero)
-    return hero
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@app.get("/heroes/")
-def read_heroes(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
-    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-    return heroes
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-@app.get("/heroes/{hero_id}")
-def read_hero(hero_id: int, session: SessionDep) -> Hero:
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return hero
+def get_user_by_email(*, session: Session, email: str) -> User | None:
+    statement = select(User).where(User.email == email)
+    session_user = session.exec(statement).first()
+    return session_user
 
 
-@app.delete("/heroes/{hero_id}")
-def delete_hero(hero_id: int, session: SessionDep):
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    session.delete(hero)
-    session.commit()
-    return {"ok": True}
+def authenticate(*, session: Session, email: str, password: str) -> User | None:
+    db_user = get_user_by_email(session=session, email=email)
+    if not db_user:
+        return None
+    if not verify_password(password, db_user.password):
+        return None
+    return db_user
+
+
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm="HS256")
+    return encoded_jwt
+
+
+@app.post("/login")
+def login_access_token(
+    session: SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = authenticate(
+        session=session, email=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    return Token(
+        access_token=create_access_token(
+            user.id, expires_delta=timedelta(minutes=60)
+        )
+    )
+
+
+# @app.get("/heroes/")
+# def read_heroes(
+#     session: SessionDep,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Hero]:
+#     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+#     return heroes
+
+
+# @app.get("/heroes/{hero_id}")
+# def read_hero(hero_id: int, session: SessionDep) -> Hero:
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     return hero
+
+
+# @app.delete("/heroes/{hero_id}")
+# def delete_hero(hero_id: int, session: SessionDep):
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     session.delete(hero)
+#     session.commit()
+#     return {"ok": True}
--- app/models.py
+++ app/models.py
@@ -1,8 +1,20 @@
+from datetime import datetime
 from sqlmodel import Field, SQLModel
 
-
-class Hero(SQLModel, table=True):
+class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
-    secret_name: str
+    email: str = Field(index=True, unique=True)
+    password: str = Field()
+
+
+class ShortLink(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    slug: str = Field(index=True, unique=True)
+    original_file: str
+    filesize: int = Field()
+    created_at: datetime | None = Field()
+    fcs_version: str = Field()
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
