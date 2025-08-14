9a2cbdefdb6de2d8f5210793c58b1bacd00cef1b
Chen, Danny <3949245+danny50610@users.noreply.github.com>
2025-08-14 13:21:49 +0800

    refactor: delete unused code

--- app/main.py
+++ app/main.py
@@ -211,31 +211,3 @@ async def short_link_create(
     session.refresh(short_link)
 
     return short_link
-
-
-# @app.get("/heroes/")
-# def read_heroes(
-#     session: SessionDep,
-#     offset: int = 0,
-#     limit: Annotated[int, Query(le=100)] = 100,
-# ) -> list[Hero]:
-#     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-#     return heroes
-
-
-# @app.get("/heroes/{hero_id}")
-# def read_hero(hero_id: int, session: SessionDep) -> Hero:
-#     hero = session.get(Hero, hero_id)
-#     if not hero:
-#         raise HTTPException(status_code=404, detail="Hero not found")
-#     return hero
-
-
-# @app.delete("/heroes/{hero_id}")
-# def delete_hero(hero_id: int, session: SessionDep):
-#     hero = session.get(Hero, hero_id)
-#     if not hero:
-#         raise HTTPException(status_code=404, detail="Hero not found")
-#     session.delete(hero)
-#     session.commit()
-#     return {"ok": True}
